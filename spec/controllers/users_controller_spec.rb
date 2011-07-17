caf3f0b196e9b19f95f72103c7d473cc51937a44
Michael Peralta <michaelperalta1@gmail.com>
2011-07-16 21:50:02 -0400

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,8 +38,8 @@ describe UsersController do
 	describe "GET 'new'" do
 	  
     it "should be successful" do
-      	get :new
-      	response.should be_success
+      get :new
+      response.should be_success
     end
     
     it "should have the right title" do
@@ -67,13 +67,18 @@ describe UsersController do
         post :create, :user => @attr
         response.should render_template('new')
       end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
     end
     
     describe "success" do
       
       before(:each) do
         @attr = { :name => "New User", :email => "user@example.com",
-                  :password => "foobar", :password_confirmation => "foobar"}
+                  :password => "foobar", :password_confirmation => "foobar" }
       end
     
       it "should create a user" do
@@ -81,8 +86,13 @@ describe UsersController do
           post :create, :user => @attr
         end.should change(User, :count).by(1)
       end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     
-      it "should redirect to the user show page"
+      it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
       end
