049dca9fd909d1a9f91505aa1687494c23ed760f
Michael Peralta <michaelperalta1@gmail.com>
2011-07-29 19:06:27 -0400

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
 	
+	has_many :microposts, :dependent => :destroy
+	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
 	validates :name,  :presence => true,
@@ -68,6 +70,11 @@ class User < ActiveRecord::Base
 	def self.authenticate(email, submitted_password)
 	  user = find_by_email(email)
 	  user && user.has_password?(submitted_password) ? user: nil
+	end
+	
+	def feed
+	  # This is preliminary. See Chapter 12 for full implementation.
+	  Micropost.where("user_id = ?", id)
 	end
 	
 	private
